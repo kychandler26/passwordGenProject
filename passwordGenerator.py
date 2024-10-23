7be755a15aa66484e734919f55b3087b5d412d4e
kychandler26 <kychandler26@gmail.com>
2024-10-23 16:02:32 +1100

    First commit. Added user prompts for min chars, max chars and bools for upper and lowercase letters.

---
+++ passwordGenerator.py
@@ -0,0 +1,13 @@
+import sys
+
+# Ask the user for minimum number of characters required from the user
+max_chars = input("What is the maximum number of characters allowed in the password?: ")
+
+# Ask the user for maximum number of characters required from the user
+max_chars = input("What is the minumum number of characters allowed in the password?: ")
+
+# Ask the user if an uppercase letter is required in the password
+upper_required = input("Does the password require at least one uppercase letter (Y/N)? ")
+
+# Ask the user if a lowercase letter is required in the password
+lower_required = input("Does the password require at least one lowercase letter (Y/N)? ")
